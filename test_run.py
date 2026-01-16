b9e38d22db10c187be0ebfb15427e26d1ef9e57e
Jinkyo <jinkyo0714@naver.com>
2026-01-16 15:26:19 -0600

    Add test_run python file

---
+++ test_run.py
@@ -0,0 +1,7 @@
+import sys
+import numpy as np
+
+print("Hello Hackathon 👋")
+print("Python version:", sys.version)
+print("NumPy version:", np.__version__)
+print("Sum test:", np.sum([1, 2, 3, 4]))
