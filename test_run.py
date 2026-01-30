fa5a39e64e7eb93a8856a27617ef03e2a4919487
Jinkyo <jinkyo0714@naver.com>
2026-01-30 17:33:26 -0600

    test commit

--- test_run.py
+++ test_run.py
@@ -4,4 +4,4 @@ import numpy as np
 print("Hello Hackathon 👋")
 print("Python version:", sys.version)
 print("NumPy version:", np.__version__)
-print("Sum test:", np.sum([1, 2, 3, 4]))
+print("Sum test:", np.sum([1, 2, 3, 4, 5]))
